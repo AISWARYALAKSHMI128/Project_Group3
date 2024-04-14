673a9fe92f9d22c98b7da679429dfa7f46250537
AISWARYALAKSHMI128 <156852992+AISWARYALAKSHMI128@users.noreply.github.com>
2024-04-13 20:58:44 -0400

    Final

--- Website/app_template_detail_2.py
+++ Website/app_template_detail_2.py
@@ -2,7 +2,10 @@ from flask import Flask, render_template
 import sqlite3
 import pathlib 
 
+#basepath where the database is located
 base_path = pathlib.Path(r'C:\Users\USER\Documents\GitHub\Project_Group3\Database')
+
+#initializing flask
 app = Flask(__name__, static_url_path='/static')
 db_name = "Airlines.db"
 db_path = base_path / db_name
@@ -10,16 +13,18 @@ print(db_path)
 
 app = Flask(__name__)
 
+#defining route for home page
 @app.route("/")
 def index():
     return render_template("index_links.html") 
 
+#defining route for about page
 @app.route("/about")
 def about():
     return render_template("about.html")
 
 
-
+#defining route for data page
 @app.route("/data")
 def data():
     con = sqlite3.connect(db_path)
@@ -29,5 +34,6 @@ def data():
 
     return render_template("data_table.html", passenger=passenger)
 
+#running flask application
 if __name__=="__main__":
     app.run(debug=True)
